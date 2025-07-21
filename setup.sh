b0bf3dd9bc4ae321778b1769d7cba2a7a2c19fb4
Zain Ul Abdin <ghourimarti@gmail.com>
2025-07-21 14:57:31 +0500

    .

---
+++ setup.sh
@@ -0,0 +1,26 @@
+conda create -p llmops python
+conda activate "D:\Coursera Courses (NVME)\LLM freecodecamp\LLMOps-sunnysavita\LLMOps-RAG-With-AWS-Lambda-ECR-Docker-Langchain-Huggingface-\llmops"
+
+
+https://cloud.google.com/architecture/mlops-continuous-delivery-and-automation-pipelines-in-machine-learning
+
+
+# ===============================================================
+# install
+curl -Ls https://astral.sh/uv/install.sh | sh
+# ===============================================================
+# install uv
+python -m venv llmopss
+source llmopss/Scripts/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# =============================================================
+
+
+# install docker
+docker build -t flask-qa-app .
+
+docker run -p 5000:5000 flask-qa-app
+
